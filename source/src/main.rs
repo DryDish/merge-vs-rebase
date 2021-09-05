27e884fee533188260e09dd9f5161c4c30b79ca0
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-09-05 11:51:10 +0200

    added a cool function

--- source/src/main.rs
+++ source/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let something_to_pass = "HERE YOU GO!";
+    cool_func(something_to_pass);
+}
+
+fn cool_func(something: &str) {
+    println!("Here is your something:");
+    println!("{}, cool right?", something);
 }
