45a96e3627be204b46e31361d70890da0b027ed6
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-09-05 11:57:09 +0200

    added generic return function

--- source/src/main.rs
+++ source/src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
     let something_to_pass = "HERE YOU GO!";
+    let thing_to_return = "here we are";
+
     cool_func(something_to_pass);
+    let new_thing_returned = other_func(thing_to_return);
+    println!("new entity, returned: {}", new_thing_returned);
 }
 
 fn cool_func(something: &str) {
     println!("Here is your something:");
     println!("{}, cool right?", something);
+}
+
+fn other_func<T>(returnable: T) -> T {
+    println!("here is your thing back");
+    returnable
 }
